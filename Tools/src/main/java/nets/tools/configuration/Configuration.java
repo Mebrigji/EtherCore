3584be2ab1009e0807425250736030874d262a01
Said0ra <szytaner@gmail.com>
2024-02-21 23:01:02 +0100

    Updated

--- Tools/src/main/java/nets/tools/configuration/Configuration.java
+++ Tools/src/main/java/nets/tools/configuration/Configuration.java
@@ -4,7 +4,7 @@ import eu.okaeri.configs.OkaeriConfig;
 
 public class Configuration extends OkaeriConfig {
 
-    public String MONGO_URI = "mongodb://admin:zAFc1M%24!o86w54%2379z@212.23.222.83:25592/?authMechanism=SCRAM-SHA-1";
+    public String MONGO_URI = "";
     public String MONGO_TABLE = "EtherCraft-Guilds";
 
 
